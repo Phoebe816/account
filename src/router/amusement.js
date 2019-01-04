ed94fb87d99ad6aff20434656c6a2af791e5d692
yangsiyuan <yangsiyuan0@gmail.com>
2019-01-04 12:34:24 +0800

    清理

--- src/router/amusement.js
+++ src/router/amusement.js
@@ -1,16 +1,9 @@
-export default [  
-  { 
-    path:"/music", 
-    name:'music',
-    component:() => import('@/pages/amusement/Music'),
-    meta:{homePages:true,amusement:true}
-  },
-  
-  { 
-    path:"/timetool", 
-    name:'timetool',
-    component:() => import('@/pages/amusement/TimeTool'),
-    meta:{homePages:true,amusement:true}
-  },
- 
+export default [{
+        path: "/music",
+        name: 'music',
+        component: () =>
+            import ('@/pages/amusement/Music'),
+        meta: { homePages: true, amusement: true }
+    },
+
 ]
